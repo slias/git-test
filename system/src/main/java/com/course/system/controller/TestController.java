81f20709c3a001b1654748b0d1e8c837f7ef875c
silas <L2198921787@163.com>
2020-06-06 14:36:43 +0800

    初始

---
+++ system/src/main/java/com/course/system/controller/TestController.java
@@ -0,0 +1,21 @@
+package com.course.system.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Silas
+ * Date: 2020/6/5
+ * Time: 16:23
+ * To change this template use File | Settings | File Templates.
+ * Description:
+ */
+@RestController
+public class TestController {
+
+    @RequestMapping("/test")
+    public  String test(){
+        return "success";
+    }
+}
