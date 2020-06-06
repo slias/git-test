d6c3d5360691fe8a321e728cb55b901a798536ba
silas <L2198921787@163.com>
2020-06-06 21:51:49 +0800

    1:生产持久层代码

--- gateway/src/main/java/com/sourse/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/sourse/gateway/GatewayApplication.java
@@ -19,7 +19,7 @@ public class GatewayApplication {
 		SpringApplication app = new SpringApplication(GatewayApplication.class);
 		Environment env = app.run(args).getEnvironment();
 		LOG.info("启动成功！！");
-		LOG.info("Eureka地址: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
+		LOG.info("Gateway地址: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
 	}
 
 }
---
+++ server/src/main/java/com/course/server/mapper/ChapterMapper.java
@@ -0,0 +1,7 @@
+package com.course.server.mapper;
+
+import com.course.server.domain.Chapter;
+import com.course.server.my.mapper.MyMapper;
+
+public interface ChapterMapper extends MyMapper<Chapter> {
+}
